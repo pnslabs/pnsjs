f16ce3041824dcb03313b09086e7cf9282120393
meisterjustice <eziefulejustice@gmail.com>
2023-02-23 10:07:51 +0400

    chore: configure rollup to bundle for web & node

---
+++ rollup.config.mjs
@@ -0,0 +1,23 @@
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/bundle.cjs.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/bundle.esm.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript(), resolve(), commonjs(), json()],
+  },
+];
